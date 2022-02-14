bba059161fddc7efd3fc7373736f665a36d35f99
mc-b <marcel.bernet@ch-open.ch>
2022-02-14 15:04:23 +0100

    Detailkorrekturen

--- i2c/oled.py
+++ i2c/oled.py
@@ -2,7 +2,7 @@ from machine import Pin, SoftI2C
 from lib.oled.ssd1306 import SSD1306_I2C
 
 # I2C Bus
-i2c = SoftI2C(sda=machine.Pin(DEFAULT_IOTKIT_I2C_SDA), scl=machine.Pin(DEFAULT_IOTKIT_I2C_SCL))
+i2c = SoftI2C(sda=Pin(DEFAULT_IOTKIT_I2C_SDA), scl=Pin(DEFAULT_IOTKIT_I2C_SCL))
 display = SSD1306_I2C(128, 64, i2c)
 
 
