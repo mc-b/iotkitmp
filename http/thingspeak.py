a5c0ec1a8d191b487f37be371d12ce6f82f576b5
mc-b <marcel.bernet@ch-open.ch>
2022-02-16 18:01:36 +0100

    ThingSpeak Grundgeruest

---
+++ http/thingspeak.py
@@ -0,0 +1,24 @@
+from machine import I2C, Pin, SoftI2C
+from lib.sensors.bmp180 import BMP180
+from time import sleep
+from lib.config import *
+from lib.oled.ssd1306 import SSD1306_I2C
+import urequests
+
+# 
+i2c = SoftI2C(sda=Pin(DEFAULT_IOTKIT_I2C_SDA), scl=Pin(DEFAULT_IOTKIT_I2C_SCL))
+display = SSD1306_I2C(128, 64, i2c)
+
+bmp180 = BMP180(i2c)
+bmp180.oversample_sett = 2
+bmp180.baseline = 101325
+
+while True:
+  try:
+    temp = bmp180.temperature
+    pres = bmp180.pressure
+
+    req = urequests.request(method='POST', url='http://api.thingspeak.com/update',json={'key':'A2ABBMDJYRAMA6JM','field1':(temp),'field2':(pres)})
+  except:
+    print('failed')
+  sleep( 15 )
