4264b24ed21ec02508ef6ce9178196ff599bfd62
mc-b <marcel.bernet@ch-open.ch>
2022-02-22 14:03:15 +0100

    Beschreibung Pin-Belegungen

--- gpio/sound.py
+++ gpio/sound.py
@@ -6,7 +6,7 @@ def pulse(l, t):
         l.duty(int(math.sin(i / 10 * math.pi) * 500 + 500))
         time.sleep_ms(t)
 
-led = machine.PWM(machine.Pin(DEFAULT_IOTKIT_BUZZER), freq=100)
+led = machine.PWM(machine.Pin(26), freq=100)
 
 for i in range(10):
     pulse(led, 20)
