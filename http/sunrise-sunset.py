05f8c831284aa533f7e9a9d2225711cccc007376
mc-b <marcel.bernet@ch-open.ch>
2022-02-22 17:42:17 +0100

    Umbau auf urequests

--- http/sunrise-sunset.py
+++ http/sunrise-sunset.py
@@ -1,9 +1,8 @@
 from machine import Pin, SoftI2C
-from time import sleep
 from lib.config import *
-import json
 from lib.oled.ssd1306 import SSD1306_I2C
-import usocket as socket
+import urequests
+import json
 
 # Oled Display
 i2c = SoftI2C(sda=Pin(DEFAULT_IOTKIT_I2C_SDA), scl=Pin(DEFAULT_IOTKIT_I2C_SCL))
@@ -12,28 +11,10 @@ display = SSD1306_I2C(128, 64, i2c)
 ###
 # Verbindung zum Cloud Dienst
 #
-def getSunrise():
-    s = socket.socket()
-
-    ai = socket.getaddrinfo("api.sunrise-sunset.org", 80)
-    print("Address infos:", ai)
-    addr = ai[0][-1]
-
-    print("Connect address:", addr)
-    s.connect(addr)
-
-    s.send(b"GET /json?lat=47.3686498&lng=8.5391825 HTTP/1.1\r\nHost: api.sunrise-sunset.org\r\n\r\n")
-    rc = s.recv(4096).decode("ascii").splitlines() 
-    s.close()
-
-    for r in rc:
-        if r.startswith( '{' ):
-            return r
-    return rc            
-
+req = urequests.request(method='GET', url='http://api.sunrise-sunset.org/json?lat=47.3686498&lng=8.5391825')
 
 # Umwandlung nach JSON
-rc = getSunrise()
+rc = req.text
 data = json.loads( rc )
 results = data['results']
 sr = str(results['sunrise'])
