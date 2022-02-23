ec6f3e0fc62842a1f2afc441d083084afc7acdc9
mc-b <marcel.bernet@ch-open.ch>
2022-02-23 09:57:04 +0100

    Zustaende, Alert, RFID Abhandlung

--- mqtt/mqttpublish.py
+++ mqtt/mqttpublish.py
@@ -2,8 +2,9 @@
 # Test der MQTT Library
 
 import time
-from machine import I2C, Pin, SoftI2C
+from machine import I2C, Pin, SoftI2C, SoftSPI
 from lib.sensors.bmp180 import BMP180
+from lib.sensors.mfrc522 import MFRC522
 from lib.config import *
 from umqtt.robust import MQTTClient
 import ubinascii
@@ -20,37 +21,59 @@ topicALERT  = b"iotkit/alert"
 topicRFID   = b"iotkit/rfid"
 topicSERVO  = b"iotkit/servo"
 # MQTT Brocker
-hostname    = "cloud.tbz.ch"
+# hostname    = "cloud.tbz.ch"
+# port        = 1883
+hostname    = "3.227.202.165"
+port        = 31883
 
 # Klassifikation 
 cls       = ( "low", "middle", "high" )
 type      = 0
 
+# Temperatur Sensor
 bus = SoftI2C(sda=Pin(DEFAULT_IOTKIT_I2C_SDA), scl=Pin(DEFAULT_IOTKIT_I2C_SCL))
 
 bmp180 = BMP180(bus)
 bmp180.oversample_sett = 2
 bmp180.baseline = 101325
 
+# RFID Reader
+sck = Pin(DEFAULT_IOTKIT_SPI_SCLK)
+mosi = Pin(DEFAULT_IOTKIT_SPI_MOSI)
+miso = Pin(DEFAULT_IOTKIT_SPI_MISO)
+spi = SoftSPI(baudrate=100000, polarity=0, phase=0, sck=sck, mosi=mosi, miso=miso)
+sda = Pin(DEFAULT_IOTKIT_SPI_SS, Pin.OUT)
+rdr = MFRC522(spi, sda)
 
+# Button
+button = Pin(DEFAULT_IOTKIT_BUTTON1, Pin.IN)
+
+# MQTT Subscribe
 def sub_cb(topic, msg):
   print((topic, msg))
   if topic == b'notification' and msg == b'received':
     print('ESP received hello message')
 
+# MQTT Login
 def connect_and_subscribe():
-  global client_id, mqtt_server, topicSERVO
-  client = MQTTClient(client_id, hostname)
+  global client_id, hostname, port, topicSERVO
+  client = MQTTClient(client_id, hostname, port)
   client.set_callback(sub_cb)
   client.connect()
   client.subscribe(topicSERVO)
   print('Connected to %s MQTT broker, subscribed to %s topic' % (hostname, topicSERVO))
   return client
 
+# MQTT Restart 
 def restart_and_reconnect():
   print('Failed to connect to MQTT broker. Reconnecting...')
   time.sleep(10)
   machine.reset()
+
+
+### Hauptprogramm
+
+counter = 1
 
 try:
   client = connect_and_subscribe()
@@ -61,9 +84,32 @@ except OSError as e:
 while True:
   try:
     client.check_msg()
-    msg = "0x3c," + str(bmp180.temperature) + "," + str(bmp180.pressure / 1000)
+
+    # Temperatur
+    if counter % 3 == 1:
+        msg = "0xBC," + str(bmp180.temperature - 5) + "," + str(bmp180.pressure / 1000) + ",low"
+    if counter % 3 == 2:
+        msg = "0xBC," + str(bmp180.temperature) + "," + str(bmp180.pressure / 1000) +  ",middle"
+    if counter % 3 == 0:
+        msg = "0xBC," + str(bmp180.temperature + 5) + "," + str(bmp180.pressure / 1000) + ",high"
     client.publish(topicTEMP, msg)
-    print( topicTEMP, msg )
+    print( topicTEMP, counter, msg )
+    counter = counter + 1
+
+    # Button startet BPMN Prozess
+    if  button.value() == 0:
+        client.publish(topicALERT, "alert")
+        print( topicALERT, counter, "alert" )
+
+    uid = ""
+    (stat, tag_type) = rdr.request(rdr.REQIDL)
+    if stat == rdr.OK:
+        (stat, raw_uid) = rdr.anticoll()
+        if stat == rdr.OK:
+            uid = ("0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3]))
+            client.publish(topicRFID, uid )
+            print(topicRFID, counter, uid)
+
     time.sleep( 1.0 )
   except OSError as e:
     restart_and_reconnect()
